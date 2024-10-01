2ebeb8c23aa569139065de372ce9967946116fa6
faraalif <faralailla2154@gmail.com>
2024-10-01 09:44:29 +0700

    p

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Kelas;
+use Illuminate\Http\Request;
+use App\Models\User; 
+
+class UserController extends Controller
+{
+    public function create()
+    {
+        $kelas = Kelas::all(); // Pastikan ini mengembalikan data dari database
+        return view('create_user', ['kelas' => $kelas]);
+    }
+
+    public function store(Request $request)
+    {
+
+        $validatedData = $request->validate([
+            'nama' => 'required|string|max:255',
+            'npm' => 'required|string|max:255',
+            'kelas_id' => 'required|exists:kelas,id',
+        ]);
+
+        $user = User::create($validatedData);
+
+        $user->load('kelas');
+
+        return view('profile', [
+            'nama' => $user->nama,
+            'npm' => $user->npm,
+            'kelas_id' => $user->kelas->nama_kelas ?? 'Kelas tidak ditemukan',
+        ]);
+    }
+}
